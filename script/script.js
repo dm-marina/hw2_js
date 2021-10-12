c27c0efe49139f7890fdb2d8d6cd8ae3deb7b007
dm-marina <mysummer1404@gmail.com>
2021-10-12 16:16:13 +0300

    dm-marina

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+let a = prompt("Enter first number:");
+let b = prompt("Enter second number:");
+let addition = Number(a)+Number(b);
+console.log(`${a} + ${b} = ${addition}`);
+let subtraction = (a)-(b);
+console.log(`${a} - ${b} = ${subtraction}`);
+let multiplication = (a)*(b);
+console.log(`${a} * ${b} = ${multiplication}`);
+let division = (a)/(b);
+console.log(`${a} / ${b} = ${division}`);
